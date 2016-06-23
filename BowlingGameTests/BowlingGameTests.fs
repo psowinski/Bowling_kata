b5052421f0b7122aba4bac26aa7f2dbc02278ef0
Przemyslaw Sowinski <psowinski@users.noreply.github.com>
2016-06-23 23:15:27 +0200

    My solution of bowling kata

---
+++ BowlingGameTests/BowlingGameTests.fs
@@ -0,0 +1,47 @@
+﻿namespace BowlingKataTests
+open Xunit
+open Swensen.Unquote
+open BowlingKata.BowlingGame
+
+module BowlingGameTests =
+
+    [<Fact>]
+    let ShouldBePossibleToCreateGameWithRolls() =
+        let g = Game []
+        Assert.NotNull(g)
+
+    [<Fact>]
+    let ShouldBePossibleToParseGameResult() =
+        let rolls = parseGame "X-/1-" |> fun (Game r) -> r
+        let exp = [(Strike, Pins 10); (Roll, Pins 0); (Spare, Pins 10); (Roll, Pins 1); (Roll, Pins 0)]
+        Assert.Equal<(Rolls * Pins) list>(exp, rolls)
+
+    [<Fact>]
+    let ``calculate scores with no strikes or spares``() =
+        test <@ scoreGame (parseGame "11111111111111111111") = 20 @>
+        test <@ scoreGame (parseGame "11111111112222222222") = 30 @>
+        test <@ scoreGame (parseGame "13521") = 12 @> 
+
+    [<Fact>]
+    let ``calculate scores containing a miss``() =
+        test <@ scoreGame (parseGame "--------------------") = 0 @> 
+        test <@ scoreGame (parseGame "1-1----------------1") = 3 @> 
+        test <@ scoreGame (parseGame "9-9-9-9-9-9-9-9-9-9-") = 90 @> 
+
+    [<Fact>]
+    let ``calculate scores containing spares``() =
+        test <@ scoreGame (parseGame "5/3-----------------") = 16 @> 
+        test <@ scoreGame (parseGame "5/11------------3/11") = 26 @> 
+        test <@ scoreGame (parseGame "5/5/5/5/5/5/5/5/5/5/5") = 150 @> 
+
+    [<Fact>]
+    let ``calculate scores containing strikes``() =
+        test <@ scoreGame (parseGame "X34----------------") = 24 @> 
+        test <@ scoreGame (parseGame "XXX--------------") = 60 @> 
+        test <@ scoreGame (parseGame "XXXXXXXXX---") = 240 @> 
+        test <@ scoreGame (parseGame "XXXXXXXXXX--") = 270 @> 
+        test <@ scoreGame (parseGame "XXXXXXXXXXX-") = 290 @> 
+        test <@ scoreGame (parseGame "XXXXXXXXXXXX") = 300 @>
+        test <@ scoreGame (parseGame "XXXXXXXXXX12") = 274 @> 
+        test <@ scoreGame (parseGame "1/35XXX458/X3/23") = 160 @> 
+        test <@ scoreGame (parseGame "1/35XXX458/X3/XX6") = 189 @> 
---
+++ BowlingKata/BowlingGame.fs
@@ -0,0 +1,47 @@
+﻿namespace BowlingKata
+open System
+
+module BowlingGame =
+
+    type Rolls =
+    | Roll
+    | Strike (*X*)
+    | Spare (*/*)
+
+    type Pins =
+    | Pins of int
+
+    type Game = 
+    | Game of (Rolls * Pins) list
+
+    let parseGame result =
+        let rec calcSpares rolls = 
+            match rolls with
+            | [] -> []
+            | (Roll, Pins n)::(Spare,  _)::xs -> (Roll, Pins n)::(Spare, Pins (10 - n))::(calcSpares xs)
+            | x::xs -> x::(calcSpares xs)
+
+        result |> Seq.map (fun x -> match x with
+                                    | 'X' -> Strike, Pins 10
+                                    | '/' -> Spare, Pins -1
+                                    | '-' -> Roll, Pins 0
+                                    | v -> Roll, Pins (Int32.Parse(v.ToString())))
+        |> Seq.toList
+        |> calcSpares
+        |> Game
+
+    let scoreGame g =
+        let concatIfNotEmpty x xs = 
+            if List.isEmpty xs then xs else x @ xs
+            
+        let rec scoreRolls sum rolls =
+            match rolls with
+            | [] -> sum
+            | (Strike, Pins p1)::(r2, Pins p2)::(r3, Pins p3)::xs -> 
+                scoreRolls (sum + p1 + p2 + p3) (concatIfNotEmpty [r2, Pins p2; r3, Pins p3] xs)
+            | (Spare, Pins p1)::(r2, Pins p2)::xs -> 
+                scoreRolls (sum + p1 + p2) (concatIfNotEmpty [r2, Pins p2] xs)
+            | (_, Pins p1)::xs -> 
+                scoreRolls (sum + p1) xs
+
+        g |> fun (Game rolls) -> scoreRolls 0 rolls
